d8ca29811c27907e8baaee90637a4f271338f873
Siqi Yuan <yuansiqi0114@hotmail.com>
2020-01-27 22:41:10 -0500

    add a subdir xml2vhd for all stuff related to decoding address table and generating vhd files; Make the makefile rules more independent

---
+++ xml_regmap.mk
@@ -0,0 +1,21 @@
+#################################################################################
+# Generate MAP and PKG files from address table
+#################################################################################
+export LD_LIBRARY_PATH=/opt/cactus/lib
+SRC_PATH=src
+XML2VHD_PATH=xml_regmap
+
+SYM_LNK_XMLS = $(shell find . -type l)
+MAP_OBJS = $(patsubst %.xml, %_map.vhd, $(SYM_LNK_XMLS))
+PKG_OBJS = $(patsubst %.xml, %_PKG.vhd, $(SYM_LNK_XMLS))
+
+.PHONY: xml_regmap
+xml_regmap : $(MAP_OBJS)
+# need to extract dir name and path name here
+
+%_map.vhd %_PKG.vhd : %.xml
+	cd $(dir $<) &&\
+	../../$(XML2VHD_PATH)/generate_test_xml $(basename $(notdir $<)) &&\
+	../../$(XML2VHD_PATH)/build_vhdl_packages test.xml &&\
+	rm test.xml
+
