99af40f398e2c91b5ad9d270426c1ce0afaded41
Dan Gastler <dgastler@gmail.com>
2020-02-05 10:17:59 -0500

    Fixed multi-bit action register. Added cleaning rule for generated regmap vhdl. Removed old files.

--- xml_regmap.mk
+++ xml_regmap.mk
@@ -9,12 +9,16 @@ SYM_LNK_XMLS = $(shell find . -type l)
 MAP_OBJS = $(patsubst %.xml, %_map.vhd, $(SYM_LNK_XMLS))
 PKG_OBJS = $(patsubst %.xml, %_PKG.vhd, $(SYM_LNK_XMLS))
 
-.PHONY: xml_regmap
+.PHONY: xml_regmap clean_regmap
+
+clean_regmap:
+	@rm -rf $(MAP_OBJS) $(PKG_OBJS)
+
 xml_regmap : $(MAP_OBJS)
 # need to extract dir name and path name here
 
 %_map.vhd %_PKG.vhd : %.xml
-	cd $(dir $<) &&\
+	@cd $(dir $<) &&\
 	../../$(XML2VHD_PATH)/generate_test_xml $(basename $(notdir $<)) &&\
 	../../$(XML2VHD_PATH)/build_vhdl_packages test.xml &&\
 	rm test.xml
