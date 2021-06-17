fc265d1dbb1e7224793dc74e577615aaa62af56f
DavidLi1355 <lid1355218215@outlook.com>
2021-06-17 12:34:03 -0400

    create script for unit test

---
+++ unitTest.py
@@ -0,0 +1,25 @@
+import unittest
+import subprocess
+import build_vhdl_packages_parserNode
+from generate_vhdl import Generate_VHDL
+
+
+class UnitTest(unittest.TestCase):
+    def assertCompareFile(self, targetFile, sampleFile):
+        out = ''
+        with subprocess.Popen(["diff", targetFile, sampleFile], stdout=subprocess.PIPE) as p:
+            out = p.communicate()[0].decode("utf-8")
+
+        if len(out) != 0:
+            failMsg = '\n' + out
+            self.fail(failMsg)
+
+    def test_PLXVC(self):
+        Generate_VHDL("PLXVC", "addressTable/modules/plXVC.xml", "",
+                      None, None, False)
+        self.assertCompareFile("PLXVC_map_test.vhd", "PLXVC_map.vhd")
+        self.assertCompareFile("PLXVC_PKG_test.vhd", "PLXVC_PKG.vhd")
+
+
+if __name__ == "__main__":
+    unittest.main()
