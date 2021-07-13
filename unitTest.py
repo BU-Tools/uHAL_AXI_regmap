2b82ce55a696e15dd31cdaa7962338d2a8897108
DavidLi1355 <lid1355218215@outlook.com>
2021-07-13 10:55:54 -0400

    add unit test code for comparing uhal and custom parser

--- unitTest.py
+++ unitTest.py
@@ -7,7 +7,17 @@ from generate_vhdl import Generate_VHDL
 class UnitTest(unittest.TestCase):
     def assertCompareFile(self, targetFile, sampleFile):
         out = ''
-        with subprocess.Popen(["diff", targetFile, sampleFile], stdout=subprocess.PIPE) as p:
+        p = subprocess.Popen(
+            ["diff", targetFile, sampleFile], stdout=subprocess.PIPE)
+        out = p.communicate()[0].decode("utf-8")
+        if len(out) != 0:
+            failMsg = '\n' + out
+            self.fail(failMsg)
+        p.terminate()
+
+    def assertCompareDir(self, dir1, dir2):
+        out = ''
+        with subprocess.Popen(["diff", "-r", dir1, dir2], stdout=subprocess.PIPE) as p:
             out = p.communicate()[0].decode("utf-8")
 
         if len(out) != 0:
