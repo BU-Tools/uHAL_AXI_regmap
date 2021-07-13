df1ba6d61a36b6669fe54379ff8cd2364bc3b43b
DavidLi1355 <lid1355218215@outlook.com>
2021-07-13 11:25:57 -0400

    create files for testing parser

---
+++ test_vhdl_packages.py
@@ -0,0 +1,23 @@
+import unittest
+from build_vhdl_packages import useCustomParser, useUhalParser
+
+
+class UnitTest(unittest.TestCase):
+    def assertCompareDir(self, dir1, dir2):
+        p = subprocess.Popen(["diff", "-r", dir1, dir2],
+                             stdout=subprocess.PIPE)
+        out = p.communicate()[0].decode("utf-8")
+        if len(out) != 0:
+            failMsg = '\n' + out
+            self.fail(failMsg)
+        p.terminate()
+
+    def test_parser(self):
+        useCustomParser("addressTable/address_apollo.xml",
+                        HDLPath="CParserTest")
+        useUhalParser("addressTable/address_apollo.xml", HDLPath="UParserTest")
+        self.assertCompareDir("CParserTest", "UParserTest")
+
+
+if __name__ == "__main__":
+    unittest.main()
