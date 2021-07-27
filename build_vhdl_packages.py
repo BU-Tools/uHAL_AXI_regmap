2c55e41ef3f9e70220b6a6baec9694bd7af9b304
Andrew Peck <andrew.peck@cern.ch>
2021-07-27 10:48:29 -0400

    Use distutils string to bool instead of homebrew

--- build_vhdl_packages.py
+++ build_vhdl_packages.py
@@ -16,6 +16,9 @@ import logging
 import math
 import argparse
 import shutil
+import distutils.util
+
+
 #from tree import *  # import node,arraynode,tree
 try:
     from StringIO import StringIO  # for Python 2
@@ -44,14 +47,8 @@ EXIT_CODE_NODE_ADDRESS_ERRORS = 3
 
 
 def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+    return bool(distutils.util.strtobool(v))
+
 
 def findArrayType(n):
     if n.isArray():
