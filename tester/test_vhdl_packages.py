26da048e1a47315e848528a10409336bf065a649
Andrew Peck <andrew.peck@cern.ch>
2021-07-27 11:09:04 -0400

    Check git diff for changes in the test

--- tester/test_vhdl_packages.py
+++ tester/test_vhdl_packages.py
@@ -12,9 +12,18 @@ from build_vhdl_packages import useSimpleParser, useUhalParser
 
 
 class UnitTest(unittest.TestCase):
+
     def assertCompareDir(self, dir1, dir2):
         p = subprocess.Popen(["diff", "-r", dir1, dir2],
                              stdout=subprocess.PIPE)
+        out = p.communicate()[0].decode("utf-8")
+        if len(out) != 0:
+            failMsg = '\n' + out
+            self.fail(failMsg)
+        p.terminate()
+
+    def assertGitNoDiff(self, dirname):
+        p = subprocess.Popen(["git", "diff", dirname], stdout=subprocess.PIPE)
         out = p.communicate()[0].decode("utf-8")
         if len(out) != 0:
             failMsg = '\n' + out
@@ -30,7 +39,10 @@ class UnitTest(unittest.TestCase):
 
         useSimpleParser(test_xml=test_xml, HDLPath="CParserTest", regMapTemplate=regMapTemplate)
         useUhalParser(test_xml=test_xml, HDLPath="UParserTest", regMapTemplate=regMapTemplate)
+
         self.assertCompareDir("CParserTest", "UParserTest")
+        for i in ["CParserTest", "UParserTest"]:
+            self.assertGitNoDiff(i)
 
         os.remove(test_xml)
         #shutil.rmtree("CParserTest")
