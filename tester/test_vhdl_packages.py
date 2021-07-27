69e7d1f71f6295d451a76672a045d49f51cecb37
Andrew Peck <andrew.peck@cern.ch>
2021-07-27 11:26:52 -0400

    Work toward PEP8

--- tester/test_vhdl_packages.py
+++ tester/test_vhdl_packages.py
@@ -1,52 +1,63 @@
+""""""
 import unittest
 import subprocess
-import shutil
 import time
-import generate_test_xml
 import os
-
 import sys
+import generate_test_xml
 sys.path.append(r"..")
 
 from build_vhdl_packages import useSimpleParser, useUhalParser
 
 
 class UnitTest(unittest.TestCase):
+    """Unit test class for """
 
-    def assertCompareDir(self, dir1, dir2):
-        p = subprocess.Popen(["diff", "-r", dir1, dir2],
-                             stdout=subprocess.PIPE)
-        out = p.communicate()[0].decode("utf-8")
+    def assert_compare_dir(self, dir1, dir2):
+        """Assert that two directories are identical"""
+        process = subprocess.Popen(["diff", "-r", dir1, dir2],
+                                   stdout=subprocess.PIPE)
+        out = process.communicate()[0].decode("utf-8")
         if len(out) != 0:
-            failMsg = '\n' + out
-            self.fail(failMsg)
-        p.terminate()
+            fail_msg = '\n' + out
+            self.fail(fail_msg)
+        process.terminate()
 
-    def assertGitNoDiff(self, dirname):
-        p = subprocess.Popen(["git", "diff", dirname], stdout=subprocess.PIPE)
-        out = p.communicate()[0].decode("utf-8")
+    def assert_git_no_diff(self, dirname):
+        """Assert that a given directory does not have any changes relative to
+        what is committed in Git"""
+        process = subprocess.Popen(["git", "diff", dirname],
+                                   stdout=subprocess.PIPE)
+        out = process.communicate()[0].decode("utf-8")
         if len(out) != 0:
-            failMsg = '\n' + out
-            self.fail(failMsg)
-        p.terminate()
+            fail_msg = '\n' + out
+            self.fail(fail_msg)
+        process.terminate()
 
     def test_parser(self):
+        """Test the parser and XML to VHDL generation"""
 
-        test_xml="test_"+str(int(time.time()))+".xml"
+        test_xml = "test_" + str(int(time.time())) + ".xml"
         xmlpath = "../example_xml/MEM_TEST.xml"
-        generate_test_xml.generate_test_xml("MEM_TEST", 0x0, os.path.abspath(xmlpath), test_xml)
-        regMapTemplate="../templates/axi_generic/template_map_withbram.vhd"
 
-        useSimpleParser(test_xml=test_xml, HDLPath="CParserTest", regMapTemplate=regMapTemplate)
-        useUhalParser(test_xml=test_xml, HDLPath="UParserTest", regMapTemplate=regMapTemplate)
+        generate_test_xml.generate_test_xml("MEM_TEST", 0x0,
+                                            os.path.abspath(xmlpath), test_xml)
 
-        self.assertCompareDir("CParserTest", "UParserTest")
+        regmap_template = "../templates/axi_generic/template_map_withbram.vhd"
+
+        useSimpleParser(test_xml=test_xml, HDLPath="CParserTest",
+                        regMapTemplate=regmap_template)
+
+        useUhalParser(test_xml=test_xml, HDLPath="UParserTest",
+                      regMapTemplate=regmap_template)
+
+        self.assert_compare_dir("CParserTest", "UParserTest")
         for i in ["CParserTest", "UParserTest"]:
-            self.assertGitNoDiff(i)
+            self.assert_git_no_diff(i)
 
         os.remove(test_xml)
-        #shutil.rmtree("CParserTest")
-        #shutil.rmtree("UParserTest")
+        # shutil.rmtree("CParserTest")
+        # shutil.rmtree("UParserTest")
 
 
 if __name__ == "__main__":
